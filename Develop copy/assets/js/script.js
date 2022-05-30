ad23be44a616b81d4f574c90e9d009ac31872aa2
Kelly Wacker <kelwacker@gmail.com>
2022-05-29 20:49:10 -0400

    it works '

--- Develop copy/assets/js/script.js
+++ Develop copy/assets/js/script.js
@@ -1,8 +1,8 @@
 // Assignment code here
 var password = {
   letters: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
-  alphaLetters: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
-  numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  // alphaLetters: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+  numbers: ["1", "2", "3", "4", "5", "6", "7", "8", "9"],
   symbols: ["!", "#", "$", "%", "&", "*", "+", "-", ".", "/", " < ", "=", " > ", " ? ", "@", "~"]
 };
 
@@ -13,6 +13,9 @@ var generateBtn = document.querySelector("#generate");
 
 // password generator function
 function generatePassword() {
+  //reset settings
+  // password.reset();
+
   // prompt for password length
   var passwordLength = window.prompt("How long do you want your password? (8-128 characters)"); 
     if (passwordLength < 8 || passwordLength > 128) {
@@ -21,8 +24,6 @@ function generatePassword() {
       generatePassword();
 
     } else {
-      //prompt for capitalization 
-      var passwordAlpha = window.confirm("Do you want capital letters?")
       //prompt for numbers
       var passwordNumbers = window.confirm("Do you want to include numbers?");
       //prompt for symbols
@@ -31,21 +32,32 @@ function generatePassword() {
 
   var passwordCharacters = []
 
-  if (passwordLetters) {
-    passwordCharacters = passwordCharacters.concat(password.letters)
+  
+  passwordCharacters = passwordCharacters.concat(password.letters)
+
+  if (passwordNumbers === true) {
+    passwordCharacters = passwordCharacters.concat(passwordCharacters, password.numbers)
   }
 
-  if (passwordAlpha) {
-    passwordCharacters = passwordCharacters.concat(password.alphaLetters)
+  if (passwordSymbols === true) {
+    passwordCharacters = passwordCharacters.concat(passwordCharacters, password.symbols)
   }
 
-  if (passwordNumbers) {
-    passwordCharacters = passwordCharacters.concat(password.numbers)
-  }
+  // if (passwordLetters) {
+  //   passwordCharacters = passwordCharacters.concat(password.letters)
+  // }
 
-  if (passwordSymbols) {
-    passwordCharacters = passwordCharacters.concat(password.symbols)
-  }
+  // // if (passwordAlpha) {
+  // //   passwordCharacters = passwordCharacters.concat(password.alphaLetters)
+  // // }
+
+  // if (passwordNumbers) {
+  //   passwordCharacters = passwordCharacters.concat(password.numbers)
+  // }
+
+  // if (passwordSymbols) {
+  //   passwordCharacters = passwordCharacters.concat(password.symbols)
+  // }
 
   var randomPassword = ""
   for (var i = 0; i <= passwordLength; i++) {
@@ -53,28 +65,13 @@ function generatePassword() {
 
   };
 
+  console.log(passwordCharacters);
+  console.log(passwordNumbers);
+
   return randomPassword;
   
 
-  // if (passwordNumbers === true && passwordSymbols === true) {
-  //   return randomPassword;
-  // }
-
-  // else if (passwordNumbers === true && passwordSymbols === false) {
-  //   return randomPassword;
-  // }
-
-  // else if (passwordNumbers === false && passwordSymbols === true) {
-  //   return randomPassword;
-  // }
-
-  // else {
-  //   return randomPassword;
-  // }
-
 };
-
-
 
 // Write password to the #password input
 var writePassword = function() {
